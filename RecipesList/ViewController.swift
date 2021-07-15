375515d99c8aaa52c369bd99309d0ce5347caf46
1gr0k <43414453+1gr0k@users.noreply.github.com>
2021-07-15 10:21:55 +0400

    Initial Commit

---
+++ RecipesList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RecipesList
+//
+//  Created by Андрей Калямин on 15.07.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
